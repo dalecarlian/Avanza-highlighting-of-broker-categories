a56255ffe939ca6414de62de2f0a08c2a02e01ad
Alexander Jansson <jansson.alexander@gmail.com>
2017-04-10 20:54:11 +0200

    Avanza highlighting of broker categories

---
+++ avanza-highlight.user.js
@@ -0,0 +1,43 @@
+// ==UserScript==
+// @name Avanza highlighting of broker categories
+// @namespace http://tampermonkey.net/
+// @version 0.0.0.2
+// @description
+// @author
+// @match https://www.avanza.se/*
+// @grant none
+// ==/UserScript==
+
+(function() {
+  'use strict';
+
+  function highlightElement(e)
+  {
+    var text = e.text().trim();
+    if(text == 'AVA' || text == 'NON') {
+      e.css('background-color', 'pink');
+    } else if(text == '-') {
+      e.css('background-color', 'lightgray');
+    }
+  }
+
+  function brokerHighlighting(){
+    $('.latest_trades tr').each(function (i) {
+      highlightElement($(this).find(">:first-child"));
+      highlightElement($(this).find(">:nth-child(2)"));
+    });
+
+    setTimeout(brokerHighlighting, 1000);
+  }
+  function addEvent(element, eventName, fn) {
+    if (element.addEventListener) {
+      element.addEventListener(eventName, fn, false);
+    } else if (element.attachEvent) {
+      element.attachEvent('on' + eventName, fn);
+    }
+  }
+ 
+  addEvent(window, 'load', function() {
+    brokerHighlighting();
+  });
+})();
